10bbcfebc9b7aa6648fa2faa82f379e35728a835
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-26 21:05:00 -0800

    Learning grid

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -2,18 +2,26 @@ import styled from "styled-components";
 import Projects from "./Projects";
 const PortfolioPage = () => {
   return (
-    <div>
+    <Grid>
       <Hello>
         <p>Hello World</p>
         <p>My name is Brycen.</p>
       </Hello>
+      {/* <div> */}
       <Projects />
-    </div>
+      {/* </div> */}
+    </Grid>
   );
 };
 
 export default PortfolioPage;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+`;
+
 const Hello = styled.div`
   background-color: blue;
+  /* width: 750px; */
 `;
