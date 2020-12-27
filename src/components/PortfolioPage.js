781fe10bba696c507c8728bc0d4d27e058b8eab2
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-26 22:11:26 -0800

    do fancy grid work

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -29,44 +29,59 @@ const PortfolioPage = () => {
     //   <div className="card">11</div>
     //   <div className="card">12</div>
     // </Grid>
-    <Grid2>
-      <div className="card card-tall" style={divStyle}>
-        1
-      </div>
-      <div className="card " style={divStyle}>
-        2
-      </div>
-      <div className="card " style={divStyle}>
-        3
-      </div>
-      <div className="card card-wide" style={divStyle}>
-        4
-      </div>
-      <div className="card " style={divStyle}>
-        5
-      </div>
-      <div className="card " style={divStyle}>
-        6
-      </div>
-      <div className="card card-wide" style={divStyle}>
-        7
-      </div>
-      <div className="card " style={divStyle}>
-        8
-      </div>
-      <div className="card card-tall" style={divStyle}>
-        9
-      </div>
-      <div className="card " style={divStyle}>
-        10
-      </div>
-      <div className="card " style={divStyle}>
-        11
-      </div>
-      <div className="card card-wide" style={divStyle}>
-        12
-      </div>
-    </Grid2>
+    // <Grid2>
+    //   <div className="card card-tall" style={divStyle}>
+    //     1
+    //   </div>
+    //   <div className="card " style={divStyle}>
+    //     2
+    //   </div>
+    //   <div className="card " style={divStyle}>
+    //     3
+    //   </div>
+    //   <div className="card card-wide" style={divStyle}>
+    //     4
+    //   </div>
+    //   <div className="card " style={divStyle}>
+    //     5
+    //   </div>
+    //   <div className="card " style={divStyle}>
+    //     6
+    //   </div>
+    //   <div className="card card-wide" style={divStyle}>
+    //     7
+    //   </div>
+    //   <div className="card " style={divStyle}>
+    //     8
+    //   </div>
+    //   <div className="card card-tall" style={divStyle}>
+    //     9
+    //   </div>
+    //   <div className="card " style={divStyle}>
+    //     10
+    //   </div>
+    //   <div className="card " style={divStyle}>
+    //     11
+    //   </div>
+    //   <div className="card card-wide" style={divStyle}>
+    //     12
+    //   </div>
+    // </Grid2>
+    <Grid3>
+      <div className="card">a</div>
+      <div className="card">b</div>
+      <div className="card">c</div>
+      <div className="card">d</div>
+      <div className="card">e</div>
+      <div className="card">f</div>
+      <div className="card">g</div>
+      <div className="card">h</div>
+      <div className="card">i</div>
+      <div className="card">j</div>
+      <div className="card">k</div>
+      <div className="card">l</div>
+      <div className="card">main</div>
+    </Grid3>
   );
 };
 
@@ -128,6 +143,87 @@ const Grid2 = styled.div`
     .card-wide {
       grid-column: span 2 / auto;
     }
+  }
+`;
+
+const Grid3 = styled.div`
+  display: grid;
+  gap: 1rem;
+  /* Explicit Grid */
+  grid-template-areas:
+    "a b c d"
+    "l 👩 👩 e"
+    "k 👩 👩 f"
+    "j i h g";
+
+  grid-template-rows: repeat(4, 25%);
+  grid-template-columns: 240px auto auto 240px;
+
+  .card:nth-child(1) {
+    grid-area: a;
+  }
+  .card:nth-child(2) {
+    grid-area: b;
+  }
+  .card:nth-child(3) {
+    grid-area: c;
+  }
+  .card:nth-child(4) {
+    grid-area: d;
+  }
+  .card:nth-child(5) {
+    grid-area: e;
+  }
+  .card:nth-child(6) {
+    grid-area: f;
+  }
+  .card:nth-child(7) {
+    grid-area: g;
+  }
+  .card:nth-child(8) {
+    grid-area: h;
+  }
+  .card:nth-child(9) {
+    grid-area: i;
+  }
+  .card:nth-child(10) {
+    grid-area: j;
+  }
+  .card:nth-child(11) {
+    grid-area: k;
+  }
+  .card:nth-child(12) {
+    grid-area: l;
+  }
+
+  .card:last-child {
+    grid-area: 👩;
+  }
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: #353535;
+    font-size: 3rem;
+    color: #fff;
+    box-shadow: rgba(3, 8, 20, 0.1) 0px 0.15rem 0.5rem,
+      rgba(2, 8, 20, 0.1) 0px 0.075rem 0.175rem;
+    height: 100%;
+    width: 100%;
+    border-radius: 4px;
+    transition: all 500ms;
+    overflow: hidden;
+
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  .card:hover {
+    box-shadow: rgba(2, 8, 20, 0.1) 0px 0.35em 1.175em,
+      rgba(2, 8, 20, 0.08) 0px 0.175em 0.5em;
+    transform: translateY(-3px) scale(1.1);
   }
 `;
 
