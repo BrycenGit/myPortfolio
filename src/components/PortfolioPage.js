91abff95d461d784600ec71b787e766f2a48db25
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-26 21:16:06 -0800

    implement responsive grid

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -2,15 +2,27 @@ import styled from "styled-components";
 import Projects from "./Projects";
 const PortfolioPage = () => {
   return (
-    <Grid>
-      <Hello>
-        <p>Hello World</p>
-        <p>My name is Brycen.</p>
-      </Hello>
-      {/* <div> */}
-      <Projects />
-      {/* </div> */}
-    </Grid>
+    // <Grid>
+    //   <Hello>
+    //     <p>Hello World</p>
+    //     <p>My name is Brycen.</p>
+    //   </Hello>
+    //   <div>
+    //     <Projects />
+    //   </div>
+    //   <div className="card">1</div>
+    //   <div className="card">2</div>
+    //   <div className="card">3</div>
+    //   <div className="card">4</div>
+    //   <div className="card">5</div>
+    //   <div className="card">6</div>
+    //   <div className="card">7</div>
+    //   <div className="card">8</div>
+    //   <div className="card">9</div>
+    //   <div className="card">10</div>
+    //   <div className="card">11</div>
+    //   <div className="card">12</div>
+    // </Grid>
   );
 };
 
@@ -18,7 +30,19 @@ export default PortfolioPage;
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: 2fr 1fr;
+  gap: 1rem;
+
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+
+  grid-template-columns: repeat(12, 1fr);
+
+  grid-template-columns: repeat(12, minmax(240px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+
+  .card {
+    background-color: lightblue;
+    text-align: center;
+  }
 `;
 
 const Hello = styled.div`
