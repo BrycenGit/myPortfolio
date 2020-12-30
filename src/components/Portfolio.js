79c0344122060a0e0a3622affd5d6b17d227321e
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-30 13:22:38 -0800

    remove header and footer from portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,12 +9,18 @@ import styled from "styled-components";
 const Portfolio = () => {
   return (
     <Wrapper>
-      <div className="navBar">NavBar</div>
+      {/* <div className="navBar">NavBar</div> */}
       <div className="about">About</div>
 
       <div className="projects">Projects</div>
-      <div className="footer">footer</div>
-      <div className="pictureDiv">
+      {/* <div className="footer">footer</div> */}
+      <div className="pictureAbout">
+        <img
+          className="picture"
+          src="https://s3.amazonaws.com/sfc-datebook-wordpress/wp-content/uploads/sites/2/2020/06/MER6619086644d19a67a11328642904d_bears0630-1024x576.jpg"
+        />
+      </div>
+      <div className="pictureProjects">
         <img
           className="picture"
           src="https://s3.amazonaws.com/sfc-datebook-wordpress/wp-content/uploads/sites/2/2020/06/MER6619086644d19a67a11328642904d_bears0630-1024x576.jpg"
@@ -28,31 +34,35 @@ export default Portfolio;
 
 // styled Components
 const Wrapper = styled.div`
+  min-height: 100%;
   display: grid;
   /* grid-template-columns: 1fr 1fr 1fr 1fr; */
-  grid-auto-rows: 240px;
+  /* grid-auto-rows: 240px; */
   gap: 1rem;
   grid-template-areas:
-    "a a a a"
+    /* "a a a a" */
     "b b b b"
     "i i i i"
     "c c c c"
-    "d d d d";
+    "i2 i2 i2 i2";
+  /* "d d d d"; */
   grid-template-columns: repeat(4, 25%);
-  /* grid-auto-rows: 240px; */
+  grid-template-rows: 1fr 1fr 1fr 1fr;
 
   @media screen and (min-width: 600px) {
     grid-template-areas:
-      "a a a a"
+      /* "a a a a" */
       "b b i i"
-      ". . c c"
-      "d d d d";
+      "i2 i2 c c";
+    /* "d d d d"; */
+    grid-template-rows: 1fr 1fr;
   }
 
-  .navBar {
+  /* .navBar {
     grid-area: a;
     background-color: blue;
-  }
+    height: 45px;
+  } */
 
   .about {
     grid-area: b;
@@ -64,13 +74,17 @@ const Wrapper = styled.div`
     background-color: purple;
   }
 
-  .footer {
+  /* .footer {
     grid-area: d;
     background-color: green;
-  }
+  } */
 
-  .pictureDiv {
+  .pictureAbout {
     grid-area: i;
+    /* background-color: orange; */
+  }
+  .pictureProjects {
+    grid-area: i2;
     /* background-color: orange; */
   }
 
