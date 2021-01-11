bf4d4400a87b0943a258b773795fb66729229d2f
Brycen Bartolome <brycenbartolome@gmail.com>
2021-01-10 22:22:16 -0800

    add email form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,69 @@
 import styled from "styled-components";
+import emailjs from "emailjs-com";
 
-const Contact = () => {
+// import "./ContactUs.css";
+
+export default function ContactUs() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_spnis32",
+        "template_esy5g8g",
+        e.target,
+        "user_xOdVatDih7FJ3t5uSPTHa"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
+
   return (
     <Container>
-      <div>Contact</div>
+      <form className="contact-form" onSubmit={sendEmail}>
+        <input type="hidden" name="contact_number" />
+        <div>
+          <label>Name</label>
+        </div>
+        <div>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label>Email</label>
+        </div>
+        <div>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label>Subject</label>
+        </div>
+        <div>
+          <input type="text" name="subject" />
+        </div>
+        <div>
+          <label>Message</label>
+        </div>
+        <div>
+          <textarea name="message" />
+        </div>
+        <div>
+          <input type="submit" value="Send" />
+        </div>
+      </form>
     </Container>
   );
-};
+}
 
-export default Contact;
-
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
